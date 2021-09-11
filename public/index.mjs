7b2875602397678c49c5468ffc5a37af62adb012
Chathura <c.rathnayake97@gmail.com>
2021-09-11 19:37:10 +0530

    RasPi automation phase 02
    Implemented the methods to record the live stream, automated the command to start the recording

--- public/index.mjs
+++ public/index.mjs
@@ -1,64 +1,73 @@
-//the broadcaster's code
-window.onload = () => {
-    document.getElementById("my-button").onclick = () => {
+let constraintObj = { video: true }; //contraints for getUserMedia object
+
+navigator.mediaDevices
+  .getUserMedia(constraintObj)
+  .then(function (mediaStreamObj) {
+    //retrieving the video from camera
+    let mediaRecorder = new MediaRecorder(mediaStreamObj);
+    let chunks = []; //variable to store the recorded video
+
+    //start the recodring
+    mediaRecorder.start();
+
+    //code to broadcast the stream to mobile and web
+    function webRTC() {
+      async function init() {
+        const peer = createPeer();
+        mediaStreamObj
+          .getTracks()
+          .forEach((track) => peer.addTrack(track, mediaStreamObj));
+      }
+
       init();
+
+      function createPeer() {
+        const peer = new RTCPeerConnection();
+        peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer); //because broadcaster is the one who offers
+        return peer;
+      }
+
+      async function handleNegotiationNeededEvent(peer) {
+        //set broadcaster's offer
+        const offer = await peer.createOffer();
+        await peer.setLocalDescription(offer);
+        const payload = {
+          sdp: peer.localDescription,
+        };
+        const { data } = await axios.post("/broadcast", payload); //axios is used to make a post request to "/broadcast" including my(broadcaster's) offer
+        const desc = new RTCSessionDescription(data.sdp); //retrieve the server SDP
+        peer.setRemoteDescription(desc).catch((e) => console.log(e)); //set it as remote SDP
+      }
+    }
+
+    //execute the webRTC code
+    webRTC();
+
+    //function to wait 5 seconds (for testing purposes)
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    const letsWait = async () => {
+      await delay(5000);
+      console.log("Waited 5s");
+      mediaRecorder.stop(); //stop the recording
     };
-  };
-  
-  async function init() {
-    //The await expression - causes async function execution to pause until a Promise is fulfilled or rejected
-    //getting the video feed from the raspberryPi camera
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    // .then(() => {
-    //   console.log("all good");
-    // })
-    // .catch((error) => console.log(error));
-    document.getElementById("video").srcObject = stream; //send it to the tag with id "video"
-    const peer = createPeer();
-    stream.getTracks().forEach((track) => peer.addTrack(track, stream));
-  
-    /*
-    Note: Adding a track to a connection triggers renegotiation by firing a negotiationneeded event.
-    addTrack() - The RTCPeerConnection method addTrack adds a new media track to the set of tracks which
-    will be transmitted to the other peer. 
-    */
-  
-    // navigator.mediaDevices
-    //   .getUserMedia({ video: true, audio: true })
-    //   .then((stream) => {
-    //     /* use stream here */
-    //   })
-    //   .catch((error) => console.log(error));
-  }
-  
-  function createPeer() {
-    const peer = new RTCPeerConnection();
-    //   {
-    //   iceServers: [
-    //     {
-    //       urls: "stun:stun.stunprotocol.org:3478",
-    //     },
-    //   ],
-    // }
-  
-    /*
-    A negotiationneeded event is sent to the RTCPeerConnection when negotiation of the connection through the signaling channel is required.
-    In here, function handleNegotiationNeededEvent is called to handle the negotiationneeded event when it occurs.
-    */
-  
-    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer); //because broadcaster is the one who offers
-    return peer;
-  }
-  
-  async function handleNegotiationNeededEvent(peer) {
-    //set broadcaster's offer
-    const offer = await peer.createOffer();
-    await peer.setLocalDescription(offer);
-    const payload = {
-      sdp: peer.localDescription,
+
+    letsWait(); //let's wait 5 seconds
+
+    //storing the video recording in chunks variable
+    mediaRecorder.ondataavailable = function (ev) {
+      chunks.push(ev.data);
     };
-    const { data } = await axios.post("/broadcast", payload); //axios is used to make a post request to "/broadcast" including my(broadcaster's) offer
-    const desc = new RTCSessionDescription(data.sdp); //retrieve the server SDP
-    peer.setRemoteDescription(desc).catch((e) => console.log(e)); //set it as remote SDP
-  }
-  
+
+    //this runs whenever the recording of the video get stopped
+    mediaRecorder.onstop = (ev) => {
+      let blob = new Blob(chunks, { type: "video/webm;" });
+      chunks = [];
+
+      //upload the recorded video to the firebase storage
+      console.log("done");
+    };
+  })
+  .catch(function (err) {
+    console.log(err.name, err.message);
+  });
